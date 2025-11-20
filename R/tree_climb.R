9eaee3e95876c7ea20b9d67ee9a5dff0d5635349
jtlandis <jtlandis314@gmail.com>
2025-11-20 09:56:44 -0500

    Add climb functions and export climb

---
+++ R/tree_climb.R
@@ -0,0 +1,88 @@
+#' @export
+climb <- function(.data, method = c("strict", "ordered_pval"), ...) {
+  method <- match.arg(method, c("strict", "ordered_pval"))
+  f <- switch(method,
+    strict = climb_strict,
+    ordered_pval = climb_pvalue_arrange
+  )
+
+  f(.data, ...)
+}
+
+#' @description
+#' climb an expanded results data with a tree. This function seems to require
+#' that the data is ordered by the hclust node order.
+#' @returns logical vector of which nodes to keep
+climb_strict <- function(tree_vec, pvals, ...) {
+  .clust <- tree(tree_vec) # should error if not length 1
+  stopifnot(
+    "`tree_vec` and `pvals` should be same size" = length(tree_vec) ==
+      length(pvals)
+  )
+  m <- .clust$merge
+  o <- .clust$order
+  n <- nrow(m) + 1
+  is_merge <- m < 0
+  m[is_merge] <- match(-m[is_merge], o)
+  m[!is_merge] <- m[!is_merge] + n
+  # pvals <- .data$pvalue
+  keep <- rep(NA, length(pvals))
+  is_na <- is.na(pvals)
+  pvals[is_na] <- 1
+  for (i in seq_len(nrow(m))) {
+    nodes <- m[i, ]
+
+    parent_node <- i + n
+    # Before we check p-values, check if we need to keep
+    # climbing. Specifically if the parent is FALSE
+    # we do not need to compare anything.
+    dont_use <- vapply(keep[nodes], isFALSE, logical(1))
+    if (any(dont_use)) {
+      keep[parent_node] <- FALSE
+      # some data may be newly merged and havent been tested
+      # we don't need to compare p-values for these if we know
+      # this merge index wont be used
+      keep[nodes[!dont_use]] <- TRUE
+      next
+    }
+
+    node_p <- pvals[nodes]
+    if (all(node_p > pvals[parent_node])) {
+      keep[nodes] <- FALSE
+      keep[parent_node] <- TRUE
+    } else {
+      keep[nodes] <- TRUE
+      keep[parent_node] <- FALSE
+    }
+  }
+
+  # .data$pvalue[is_na] <- NA_real_
+  keep
+}
+
+#' @param .data a result object
+#' @param ... unused
+climb_pvalue_arrange <- function(tree_vec, pvals, ..., merge_alpha = 1) {
+  n_leafs <- length(tree(tree_vec)$order)
+
+  # leafs <- filter(res, n_children==1) |>
+  #   tidyr::unnest(descendants)
+  desc <- numeric()
+  o_pval <- order(pvals)
+  keep <- logical(length(tree_vec))
+  pvals <- pvals[o_pval]
+  tree_vec <- tree_vec[o_pval]
+  i <- 0L
+  theDesc <- descendants(tree_vec)
+  nchild <- vapply(theDesc, length, integer(1))
+  while (length(desc) < n_leafs) {
+    i <- i + 1L
+    desc2 <- theDesc[[i]]
+    simi <- any(desc2 %in% desc)
+    if (!simi && (nchild[i] == 1L || pvals[i] < merge_alpha)) {
+      desc <- c(desc, desc2)
+      keep[i] <- TRUE
+    }
+  }
+  keep[order(o_pval)]
+}
