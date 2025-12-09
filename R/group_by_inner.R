50c4d00d6b28f64e6f9e9f76f9fb9b6a79453f31
jtlandis <jtlandis314@gmail.com>
2025-12-08 22:13:14 -0500

    adjusting the arguments

--- R/group_by_inner.R
+++ R/group_by_inner.R
@@ -1,15 +1,16 @@
 #' @export
-group_by_inner <- function(x, .by, node_level = NULL) {
+group_by_inner <- function(x, .by, ...) {
   UseMethod("group_by_inner")
 }
 
 #' @export
-group_by_inner.data.frame <- function(x, .by, node_level = NULL) {
+group_by_inner.data.frame <- function(x, .by, ..., node_level = NULL) {
   by_tree <- dplyr::select(x, {{ .by }})
   stopifnot(
     "`.by` should only select one variable" = length(by_tree) == 1L,
     "`.by` should be a 'tree_vctr'" = inherits(by_tree[[1]], "tree_vctr")
   )
+  node_level <- rlang::eval_tidy({{ node_level }}, data = x)
   groups <- generate_inner_slice(by_tree[[1]], node_level = node_level) |>
     dplyr::rename(
       "{names(by_tree)}" := nodes,
@@ -19,16 +20,37 @@ group_by_inner.data.frame <- function(x, .by, node_level = NULL) {
 }
 
 #' @export
-group_by_inner.PlySummarizedExperiment <- function(x, .by, node_level = NULL) {
+group_by_inner.PlySummarizedExperiment <- function(
+  x, .by, ...,
+  row_node_level = NULL, col_node_level = NULL
+) {
   by_tree <- dplyr::select(x, {{ .by }})
   row_data <- SummarizedExperiment::rowData(by_tree)
   nrow_sel <- length(row_data)
+  quos <- plyxp:::plyxp_quos(
+    {{ row_node_level }},
+    {{ col_node_level }},
+    .ctx = c("assays", "rows", "cols")
+  )
+  mask <- plyxp:::new_plyxp_manager(plyxp::se(x))
+  ctxs <- vapply(quos, attr, FUN.VALUE = "", which = "plyxp:::ctx")
+  nms <- names(quos)
+  mask <- plyxp:::plyxp_evaluate(mask, quos, ctxs, nms, rlang::caller_env())
+  results <- mask$results()
+  mask <- plyxp:::new_plyxp_manager(plyxp::se(x))
   if (nrow_sel != 0) {
     stopifnot(
       "`.by` should only select one rowData variable" = nrow_sel == 1L,
       "`.by` should be a 'tree_vctr'" = inherits(row_data[[1]], "tree_vctr")
     )
-    row_data <- generate_inner_slice(row_data[[1]], node_level = node_level) |>
+    node_level <- if (length(results$rows)) {
+      results$rows[[1]]
+    } else {
+      NULL
+    }
+    row_data <- generate_inner_slice(row_data[[1]],
+      node_level = node_level
+    ) |>
       dplyr::rename(
         "{names(row_data)}" := nodes,
         .indices = children
@@ -45,9 +67,16 @@ group_by_inner.PlySummarizedExperiment <- function(x, .by, node_level = NULL) {
       "`.by` should only select one colData variable" = ncol_sel == 1L,
       "`.by` should be a 'tree_vctr'" = inherits(col_data[[1]], "tree_vctr")
     )
-    col_data <- generate_inner_slice(col_data[[1]], node_level = node_level) |>
+    node_level <- if (length(results$cols)) {
+      results$cols[[1]]
+    } else {
+      NULL
+    }
+    col_data <- generate_inner_slice(col_data[[1]],
+      node_level = node_level
+    ) |>
       dplyr::rename(
-        "{names(row_data)}" := nodes,
+        "{names(col_data)}" := nodes,
         .indices = children
       ) |>
       dplyr::mutate(.indices_group_id = seq_len(dplyr::n())) |>
